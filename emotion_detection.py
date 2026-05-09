1f55ac6d909b3f3860c38e7c32d90d98743afef8
knyongki <yyongkisetiawan@gmail.com>
2026-05-09 10:51:57 -0400

    Ekstrak kumpulan emosi yang diperlukan, termasuk kemarahan, jijik, ketakutan, kegembiraan, dan kesedihan, beserta skor

--- emotion_detection.py
+++ emotion_detection.py
@@ -16,5 +16,26 @@ def emotion_detector(text_to_analyze):
     }
     
     response = requests.post(url, json=input_json, headers=headers)
-    
-    return response.text
+
+    formatted_response = response.json()
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger = emotions["anger"]
+    disgust = emotions["disgust"]
+    fear = emotions["fear"]
+    joy = emotions["joy"]
+    sadness = emotions["sadness"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    result = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
+
+    return result
